ca21def5086ea805ddaafecb04d7e724c6bd6b3f
esterlign <esterling@brainbytescreative.com>
2017-05-03 19:51:16 -0400

    JAWSDB Database

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,25 @@
-// var mysql      = require('mysql');
-// var connection = mysql.createConnection({
-//     host     : 'localhost',
-//     user     : 'root',
-//     password : 'Septembre1',
-//     database : 'burgers_db'
-// });
-//
-// connection.connect();
+var mysql      = require('mysql');
+var connection;
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+    connection = mysql.createConnection({
+        host     : 'localhost',
+        user     : 'root',
+        password : 'Septembre1',
+        database : 'burgers_db'
+    });
+}
 
+
+connection.connect();
+//
 // connection.query('SELECT * FROM burgers', function (error, results, fields) {
 //     if (error) throw error;
 //     console.log('The solution is: ', results);
 // });
+//
+// connection.end();
 
-
-
-// module.exports = connection;
+module.exports = connection;
--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -3,16 +3,14 @@ var express = require("express");
 
 var router = express.Router();
 
-//var orm = require("../config/orm");
+var orm = require("../config/orm");
 
 
 router.get("/", function (req, res) {
-    // orm.selectAll(function (result) {
-    //     res.render("index", {result: result});
-    //
-    // });
+    orm.selectAll(function (result) {
+        res.render("index", {result: result});
 
-    res.render("index");
+    });
 
 });
 
@@ -20,17 +18,17 @@ router.put("/add", function (req, res) {
     var data = req.body;
     var date = new Date();
 
-   // orm.insertOne(data.burger_name, true, date);
+    orm.insertOne(data.burger_name, true, date);
     res.redirect("/");
 });
 
 
 
 router.put("/:id", function (req, res) {
-    // var id = req.params.id;
-    // console.log(id);
-    //
-    // orm.updateOne(false, id);
+    var id = req.params.id;
+    console.log(id);
+
+    orm.updateOne(false, id);
     res.redirect("/");
 
 });
